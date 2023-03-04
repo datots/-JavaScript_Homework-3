94e35fde3f37e9ec450e694f0aa71caa28b87def
datots <dato_ts@hotmail.com>
2023-03-05 01:20:07 +0400

    JS third homework

---
+++ skill.js
@@ -0,0 +1,45 @@
+// // function Mul_Sum(num1,num2,...numbers){
+// //     let sum = num1 + num2;
+// //     let mult = 1;
+
+// //     for (let num of numbers){
+// //         mult *= num;
+// //     }
+
+// //     return {
+// //         "Jami":sum,
+// //         "Namravli":mult
+// //     };
+// // }
+
+// // console.log(Mul_Sum(15,58,5,5));
+
+
+
+// const user = {
+//     banks:[
+//         {},
+//         {},
+//         {address:{city:"Tbilisi"}}
+//     ]
+// };
+
+// const{banks,fullName} = user;
+// console.log(user.banks[2].address.city,fullName);
+
+// const simp_array = {
+//     name:"Jibgira",
+//     username:"Jibgirovich",
+//     city:"Chichotka",
+//     country:"Gvatemala",
+//     planet:"Mars"
+
+// }
+
+// const user2 = {
+//     ...simp_array
+// };
+
+
+// console.log(simp_array.country,user2.planet);
+
